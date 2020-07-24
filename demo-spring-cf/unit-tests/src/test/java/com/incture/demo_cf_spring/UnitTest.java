04f10f725843d784d73d40928ce2fd6849bd71d0
j.rohit <a-r.jalagadugula@paccar.com>
2020-07-24 12:36:23 +0530

    Initial Commit

---
+++ demo-spring-cf/unit-tests/src/test/java/com/incture/demo_cf_spring/UnitTest.java
@@ -0,0 +1,12 @@
+package com.incture.demo_cf_spring;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
